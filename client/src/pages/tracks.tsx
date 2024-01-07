e1d391e08283de6cfaf9b495548eb8d65d064a14
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2024-01-07 13:12:56 +0100

    create query to get mocked data

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 import { Layout } from "../components";
+import {gql} from "../__generated__/";
+
+// GQL constants are ALL_CAPS
+const TRACKS = gql(`
+  # Query ges here
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
 
 /**
  * Tracks Page is the Catstronauts home page.
