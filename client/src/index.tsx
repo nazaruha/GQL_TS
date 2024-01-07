ac69d38ebad383991fa655be6a3c63764d07e657
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2024-01-07 12:32:30 +0100

    apollo client setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+
+const client = new ApolloClient({ // ApolloClient is the class that represents Apollo Client itself
+    // options go here
+    uri: 'http://localhost:4000', // location of our apollo server
+    cache: new InMemoryCache(), // for in-memory cache
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}> {/* allows React component to use ApolloClient */}
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
